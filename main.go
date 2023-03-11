ac50eb8206f6fc554e41edb5980aa1778119eeca
Dowon Oh <5d1@hits.ai>
2023-03-12 00:46:02 +0900

    bind query string or post data

--- main.go
+++ main.go
@@ -1,60 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+)
 
-type StructA struct {
-	FieldA string `form:"field_a"`
-}
-
-type StructB struct {
-	NestedStruct StructA
-	FieldB       string `form:"field_b"`
-}
-
-type StructC struct {
-	NestedStructPointer *StructA
-	FieldC              string `form:"field_c"`
-}
-
-type StructD struct {
-	NestedAnonyStruct struct {
-		FieldX string `form:"field_x"`
-	}
-	FieldD string `form:"field_d"`
-}
-
-func GetDataB(c *gin.Context) {
-	var b StructB
-	c.Bind(&b)
-	c.JSON(200, gin.H{
-		"a": b.NestedStruct,
-		"b": b.FieldB,
-	})
-}
-
-func GetDataC(c *gin.Context) {
-	var b StructC
-	c.Bind(&b)
-	c.JSON(200, gin.H{
-		"a": b.NestedStructPointer,
-		"c": b.FieldC,
-	})
-}
-
-func GetDataD(c *gin.Context) {
-	var b StructD
-	c.Bind(&b)
-	c.JSON(200, gin.H{
-		"x": b.NestedAnonyStruct,
-		"d": b.FieldD,
-	})
+type Person struct {
+	Name     string    `form:"name"`
+	Address  string    `form:"address"`
+	Birthday time.Time `form:"birthday" time_format:"2006-01-02" time_utc:"1"`
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/getb", GetDataB)
-	r.GET("/getc", GetDataC)
-	r.GET("/getd", GetDataD)
+	route := gin.Default()
+	route.GET("/testing", startPage)
+	route.Run(":8085")
+}
 
-	r.Run()
+func startPage(c *gin.Context) {
+	var person Person
+
+	if c.ShouldBind(&person) == nil {
+		log.Println(person)
+	}
+
+	c.String(200, "Success")
 }
