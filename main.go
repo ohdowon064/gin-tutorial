af7a2fffe4ddb9313c8755421bdd05820eff3c8a
Dowon Oh <5d1@hits.ai>
2023-03-12 18:24:14 +0900

    Controlling Log output coloring

--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-type Person struct {
-	ID   string `uri:"id" binding:"required,uuid"`
-	Name string `uri:"name"  binding:"required"`
-}
-
 func main() {
-	route := gin.Default()
-	route.GET("/:name/:id", func(c *gin.Context) {
-		var person Person
-		if err := c.ShouldBindUri(&person); err != nil {
-			c.JSON(400, gin.H{"msg": err})
-			return
-		}
-		c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
+	gin.ForceConsoleColor()
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
 	})
-	route.Run(":8080")
+
+	router.Run(":8080")
 }
