166c72654068cc0db858b8049782ee7b317f3031
Dowon Oh <5d1@hits.ai>
2023-03-12 18:32:34 +0900

    custom HTTP configuration

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
 func main() {
-	gin.ForceConsoleColor()
-
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 
-	router.Run(":8080")
+	s.ListenAndServe()
 }
